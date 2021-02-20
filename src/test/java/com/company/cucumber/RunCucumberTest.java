225e21b13946c19e3e4662a83be953c1ad07342d
Jose Hurtado <josehumberto17@gmail.com>
2021-02-20 16:27:52 -0500

    Add report html

--- src/test/java/com/company/cucumber/RunCucumberTest.java
+++ src/test/java/com/company/cucumber/RunCucumberTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {
         "pretty",
-        "html:target/cucumber-reports/cucumber-pretty",
+        "html:target/cucumber-reports/cucumber-pretty/index.html",
         "json:target/cucumber-reports/json-reports/Cucumber.json",
         "junit:target/cucumber-reports/junit-reports/Cucumber.xml"},
         tags = "@rest",
