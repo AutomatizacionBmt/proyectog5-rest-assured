cd28556a5883be4a53148272038186409a3cc0af
Jose Hurtado <josehumberto17@gmail.com>
2021-02-20 16:14:58 -0500

    Agregar cucumber report

--- src/test/java/com/company/cucumber/RunCucumberTest.java
+++ src/test/java/com/company/cucumber/RunCucumberTest.java
@@ -1,6 +1,21 @@
 package com.company.cucumber;
 
-public class RunCucumberTest {
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber-pretty",
+        "json:target/cucumber-reports/json-reports/Cucumber.json",
+        "junit:target/cucumber-reports/junit-reports/Cucumber.xml"},
+        tags = "@rest",
+        glue = {"com.company.cucumber"},
+        features = {"src/test/resources/features"},
+        monochrome = true,
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        publish = true)
+public class RunCucumberTest {
 
 }
